e0fd5fa878f3f5fafe854a87f871fd8bb2ef00cb
Viktor Kopp <vifactor@gmail.com>
2025-11-26 09:26:16 +0100

    Trigger search on SearchDialog::OK click
    Add cancel button to Search dialog
    
    Signed-off-by: Viktor Kopp <vifactor@gmail.com>

--- src/searchdialog.cpp
+++ src/searchdialog.cpp
@@ -140,6 +140,9 @@ CSearchDialog::CSearchDialog(QWidget *parent) :
         m_timeRangeResetNeeded = false;
     });
 
+    // OK button triggers find next
+    connect(this, &SearchDialog::accepted, this, &SearchDialog::findNextClicked);
+
     fSilentMode = !QDltOptManager::getInstance()->issilentMode();
 
     updateColorbutton();
